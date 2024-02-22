4d32ad6933d60002cc5633e5a4d8628a63e550d9
Ahmed Elfarra <ahmed.e.alfarra@gmail.com>
2024-02-22 13:14:37 +0300

    Transformation Form Initialized

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -76,7 +76,11 @@ const Sidebar = () => {
                 );
               })}
               <li className="flex-center cursor-pointer gap-2 p-4">
-                <UserButton afterSignOutUrl="/" showName />
+                <UserButton
+                  afterSignOutUrl="/"
+                  showName
+                  userProfileMode="modal"
+                />
               </li>
             </ul>
           </SignedIn>
